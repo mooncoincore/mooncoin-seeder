a94a6c5819dff95eaeed0921f5bc842dfe1a60cb
pooler <pooler@litecoinpool.org>
2017-02-14 22:14:33 +0100

    Update height requirements

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 700000 : 840000;
+    return testnet ? 2000 : 1150000;
 }
 
 std::string static inline ToString(const CService &ip) {
