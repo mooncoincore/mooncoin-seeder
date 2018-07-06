0fb657c9b0f28707944464dda73bd28751d13285
mebagger <>
2018-07-05 18:33:15 -0700

    testnet changes

--- protocol.h
+++ protocol.h
@@ -20,7 +20,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 14665 : 44664;
+    return testnet ? 14664 : 44664;
 }
 
 //
